a4aa964fa2701a92c06deb93c5b68495e27b259e
Cong Manh <manhatnpt(@gmail.com>
2025-03-06 14:57:32 +0700

    jwt

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAuthDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateAuthDto {
+    @IsNotEmpty({ message: "username khong duoc de trong" })
+    username: string;
+
+    @IsNotEmpty({ message: "password khong duoc de trong" })
+    password: string;
+}
